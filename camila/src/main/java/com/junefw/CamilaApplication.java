e0478df2db8abf9811c996af7c2a8275b0574d50
kordelee <kordelee@naver.com>
2023-09-21 13:36:17 +0900

    first

---
+++ camila/src/main/java/com/junefw/CamilaApplication.java
@@ -0,0 +1,13 @@
+package com.junefw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamilaApplication.class, args);
+	}
+
+}
