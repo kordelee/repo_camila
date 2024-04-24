fcb8df26c1bc98a0321f7cf35e948c71f27f0f16
kordelee <kordelee@naver.com>
2024-04-24 10:35:22 +0900

    add logincheck interceptor

--- camila/src/main/java/com/junefw/common/constants/Constants.java
+++ camila/src/main/java/com/junefw/common/constants/Constants.java
@@ -18,7 +18,7 @@ public final class Constants {
 	
 	public static final int SESSION_MINUTE_XDM = 30;
 	
-	public static final String URL_LOGINFORM = "/member/loginForm";
+	public static final String URL_LOGINFORM = "/v1/infra/member/signinXdmForm";
 	
 	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
 
